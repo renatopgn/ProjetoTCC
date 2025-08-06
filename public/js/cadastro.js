5a68a69780979a01b1116fbd655323ec93d69dbf
renatopgn <renatofarenzena1@gmail.com>
2025-08-06 18:02:09 -0300

    Versão atualizada TCC

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -13,3 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+  const cpfInput = document.querySelector('#cpf');
+
+  cpfInput.addEventListener('input', () => {
+    let value = cpfInput.value.replace(/\D/g, ''); // Remove tudo que não for número
+
+    if (value.length > 11) value = value.slice(0, 11); // Limita a 11 números
+
+    // Aplica a máscara
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+    cpfInput.value = value;
+  });
--- public/js/login.js
+++ public/js/login.js
@@ -7,3 +7,18 @@ togglePasswordButton.addEventListener('click', () => {
     passwordInput.type = isPassword ? 'text' : 'password';
     togglePasswordButton.textContent = isPassword ? '🙈' : '👁️'; // Alterna o ícone
 });
+
+ const cpfInput = document.querySelector('#cpf');
+
+  cpfInput.addEventListener('input', () => {
+    let value = cpfInput.value.replace(/\D/g, ''); // Remove tudo que não for número
+
+    if (value.length > 11) value = value.slice(0, 11); // Limita a 11 números
+
+    // Aplica a máscara
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+    cpfInput.value = value;
+  });
